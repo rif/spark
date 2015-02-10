1ea6b355ef4eec814727ebe3ec7de1825b10419b
Radu Ioan Fericean <radu@fericean.ro>
2015-02-10 12:01:55 +0200

    better name and extension for binary files

--- crossbuild.sh
+++ crossbuild.sh
@@ -4,12 +4,12 @@ cd /go/src/github.com/rif/spark/
 rm -rf build; mkdir build
 
 go build -v -o build/linux_amd64
-GOOS=linux GOARCH=386 go build -v -o build/linux_386
-GOOS=linux GOARCH=arm GOARM=5 go build -v -o build/linux_arm5
-GOOS=darwin GOARCH=amd64 go build -v -o build/darwin_amd64
-GOOS=darwin GOARCH=386 go build -v -o build/darwin_386
-GOOS=windows GOARCH=386 go build -v -o build/windows_386
-GOOS=windows GOARCH=amd64 go build -v -o build/windows_amd64
+GOOS=linux GOARCH=386 go build -v -o build/spark_linux_386
+GOOS=linux GOARCH=arm GOARM=5 go build -v -o build/spark_linux_arm5
+GOOS=darwin GOARCH=amd64 go build -v -o build/spark_darwin_amd64
+GOOS=darwin GOARCH=386 go build -v -o build/spark_darwin_386
+GOOS=windows GOARCH=386 go build -v -o build/spark_windows_386.exe
+GOOS=windows GOARCH=amd64 go build -v -o build/spark_windows_amd64.exe
 
 go get github.com/pwaller/goupx/
 /go/bin/goupx build/linux_amd64
