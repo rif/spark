ff780d9371ec94f6b070259565105ccd811c98fb
Radu Ioan Fericean <radu@fericean.ro>
2016-01-08 13:16:35 +0200

    small crossbuild fixes

--- crossbuild.sh
+++ crossbuild.sh
@@ -2,7 +2,7 @@
 
 rm -rf build; mkdir build
 
-go build -v -o build/linux_amd64
+go build -v -o build/spark_linux_amd64
 GOOS=linux GOARCH=386 go build -v -o build/spark_linux_386
 GOOS=linux GOARCH=arm GOARM=5 go build -v -o build/spark_linux_arm5
 GOOS=darwin GOARCH=amd64 go build -v -o build/spark_darwin_amd64
@@ -11,6 +11,6 @@ GOOS=windows GOARCH=386 go build -v -o build/spark_windows_386.exe
 GOOS=windows GOARCH=amd64 go build -v -o build/spark_windows_amd64.exe
 
 go get -h github.com/pwaller/goupx/
-goupx build/linux_amd64
+goupx build/spark_linux_amd64
 upx build/*
 chmod -R a+rw build
